0325cffafc92d7f0716da6e0dd4b46cd3894b285
Huey <87251702+yo-huey@users.noreply.github.com>
2022-01-18 18:39:10 -0800

    Add files via upload

---
+++ review_rating.py
@@ -0,0 +1,50 @@
+# Default Libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Model Libraries
+import pickle
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+import string
+import re
+
+
+def text_cleaning(text):
+    text = str(text) # Convert into string
+    text = text.lower() # Lowercasing text
+    text = re.sub(r'\d+', '', text) # Remove numbers
+    tokenizer = RegexpTokenizer(r'\w+') # tokenize text & remove punctuations
+    tokened_text = tokenizer.tokenize(text)
+    
+    text = [w for w in tokened_text if w not in stopwords.words('english')] # Remove stop words
+    lemmatizer = WordNetLemmatizer()
+    new_text = [lemmatizer.lemmatize(w) for w in text]
+    
+    return ' '.join(new_text)
+
+
+with open('rating-pipeline.pickle', 'rb') as model:
+    model = pickle.load(model)
+
+
+def classify_message(model, message):
+    label = model.predict([message])[0]
+    return label
+
+
+def run():
+    st.title("Hotel Review Rating Prediction")
+    message_text = st.text_area("Enter a review", height=300)
+
+    
+    if st.button("Predict"):
+            output = classify_message(model, message_text)
+            st.write('This is a ' + str(output) + ' star hotel review!')
+
+
+if __name__ == '__main__':
+    run()
